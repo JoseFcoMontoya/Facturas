0b24af61cdfa36703362c605d6f35c5bcad1d3fe
JOSE FRANCISCO MONTOYA FERNANDEZ <josefrancisco.montoyaf@viewnext.com>
2022-07-21 10:43:32 +0200

    Creando el ToolBar

--- Facturas/Facturas/App.xaml.cs
+++ Facturas/Facturas/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Facturas.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace Facturas
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
